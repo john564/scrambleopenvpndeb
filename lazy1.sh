b3cd16bd9a9058fa27e772a95f3ed305139f0098
john564 <john56477@gmail.com>
2014-02-04 17:23:08 +0000

    added chown

--- lazy1.sh
+++ lazy1.sh
@@ -1,5 +1,6 @@
 sudo dpkg -i openvpn-patched_2.3.2-10112013-1_armhf.deb
 sudo mv etc+init.d+openvpn /etc/init.d/openvpn
 sudo chmod +x /etc/init.d/openvpn
+sudo chown root:root /etc/init.d/openvpn
 sudo update-rc.d openvpn default
 sudo /etc/init.d/openvpn start
